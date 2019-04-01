17c8c5750d9fc38bd973fefb7fb6e7b3c6dfd644
Jansen <martijn.e.jansen@rabobank.nl>
2019-04-01 15:36:41 +0200

    use intstream

--- src/main/java/nl/demo/pdfcsvgenerator/Data.java
+++ src/main/java/nl/demo/pdfcsvgenerator/Data.java
@@ -1,7 +1,9 @@
 package nl.demo.pdfcsvgenerator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Data {
     public static List<String[]> getData(int amount) {
@@ -9,17 +11,13 @@ public class Data {
     }
 
     private static List<String[]> createData(int amount) {
-        List<String[]> out = new ArrayList<>();
+        List<String[]> out = Collections.synchronizedList(new ArrayList<>());
 
         String[] addValue = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29"};
 
-        /*IntStream.range(0, amount)
+        IntStream.range(0, amount)
                 .parallel()
-                .forEach(value -> out.add(addValue));*/
-
-        for (int i = 0; i < amount; i++) {
-            out.add(addValue);
-        }
+                .forEach(value -> out.add(addValue));
 
         return out;
     }
